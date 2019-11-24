db296ad302443cf64076a837c249cc2dfd8a9765
Saikumar <Saikumar@Saikumar-PC>
2019-11-24 16:50:29 +0530

    new printline added..

--- HelloWorldGradle/src/main/java/HelloWorldGradle/HelloWorld.java
+++ HelloWorldGradle/src/main/java/HelloWorldGradle/HelloWorld.java
@@ -7,5 +7,6 @@ public class HelloWorld {
   public static void main(String[] args) {
 	System.out.println("hello gradle...");
 	System.out.println("hi");
+	System.out.println("hello world..");
 }
 }
