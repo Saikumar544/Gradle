46b13a324306716168a1ae4ee97cc7332d900c01
Saikumar <Saikumar@Saikumar-PC>
2019-11-24 17:30:18 +0530

    added exception..

--- HelloWorldGradle/src/main/java/HelloWorldGradle/Numbers.java
+++ HelloWorldGradle/src/main/java/HelloWorldGradle/Numbers.java
@@ -3,8 +3,9 @@ package HelloWorldGradle;
 public class Numbers {
 	int x[] = { 1, 2, 3, 4, 5 };
 
-	public void display() {
+	public void display() throws Exception {
 		for (int i = 0; i < x.length; i++) {
+			if(x[i]==4) throw new Exception();
 			System.out.println("numbers is : " + x[i]);
 		}
 	}
