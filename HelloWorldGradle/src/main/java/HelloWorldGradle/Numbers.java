14a579fdc5204288e8547615720720d9437a419b
Saikumar <Saikumar@Saikumar-PC>
2019-11-24 17:21:45 +0530

    Numbers class is added..

---
+++ HelloWorldGradle/src/main/java/HelloWorldGradle/Numbers.java
@@ -0,0 +1,11 @@
+package HelloWorldGradle;
+
+public class Numbers {
+	int x[] = { 1, 2, 3, 4, 5 };
+
+	public void display() {
+		for (int i = 0; i < x.length; i++) {
+			System.out.println("numbers is : " + x[i]);
+		}
+	}
+}
